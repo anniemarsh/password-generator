638d0990c387c2e21243ca4c6cc73af894f8b948
Annie Marsh <anniemarsh@Annies-MBP.lan>
2021-10-20 00:56:32 -0400

    added logic for password length

---
+++ Develop/script.js
@@ -0,0 +1,33 @@
+// Assignment code here
+var generateBtn = document.querySelector("#generate");
+
+
+//"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+//"abcdefghijklmnopqrstuvwxyz"
+
+//@%+\\/!#$^?:,)(}{][~-_
+
+//"123456789"
+
+//generate password
+function generatePassword() {
+  //do stuff
+  const totalLength = prompt("How many characters would you like to use?");
+  if(totalLength >= 8 && totalLength <= 128) {
+    alert(`Your new password will be ${totalLength} characters long.`);
+    //(`Your new password will be` + totalLength + `characters long`)
+  }
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
