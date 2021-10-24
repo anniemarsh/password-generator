955c175064a595cbc4cf042a7862133987bf78dc
Annie Marsh <anniemarsh@Annies-MBP.lan>
2021-10-24 13:20:11 -0400

    added let statements within generate password function

--- Develop/script.js
+++ Develop/script.js
@@ -13,6 +13,7 @@ function writePassword() {
 
 }
 
+//character variables
 let lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
@@ -24,10 +25,29 @@ let randomPassword = "";
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//generate password function
 function generatePassword() {
   let passwordLength = prompt("Please enter your desired password character count between 8 - 128 characters.");
   if (!passwordLength || passwordLength <= 7 || passwordLength >= 129) {
   return alert("Your password must be between 8 -128 characters. Please enter a valid count.");
   }
 
+  //let statements to confirm characters
+
+  let useLower = confirm("Do you want to include lowercase letters?");
+  if (useLower === true) {
+  masterArray.concat(lower);
+  }
+  let useUpper = confirm("Do you want to include uppercase letters?");
+  if (useUpper === true) {
+    masterArray.concat(upper);
+  }
+  let useNumbers = confirm("Do you want to include numbers?");
+  if (useNumbers === true) {
+    masterArray.concat(numbers);
+  }
+  let useSpecial = confirm("Do you want to include special characters?");
+  if (useSpecial === true) {
+    masterArray.concat(special);
+  }
 }
